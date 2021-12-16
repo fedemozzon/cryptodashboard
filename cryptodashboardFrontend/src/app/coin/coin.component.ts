abdd17dc8555f38ba0e5b422e0801a9b4354ab1a
federmozzon <mozzonfederico@gmail.com>
2021-12-16 15:39:21 -0300

    Ahora muestro dos tipos de grafico diferente

--- cryptodashboardFrontend/src/app/coin/coin.component.ts
+++ cryptodashboardFrontend/src/app/coin/coin.component.ts
@@ -26,6 +26,7 @@ export class CoinComponent implements OnInit {
     idCoin:''
   }
   graph: any
+  graph2: any
 
   
   constructor(private route: ActivatedRoute,private service: CoinService, private quotationsService: QuotationService) { }
@@ -33,11 +34,25 @@ export class CoinComponent implements OnInit {
   ngOnInit(): void {
     this.idCoin = this.route.snapshot.paramMap.get("idx") as string
     this.service.getCoin(this.idCoin).subscribe((coin) => this.coin = coin)
-    this.quotationsService.getQuotations().subscribe((quotations) => {this.results = quotations.filter((quotation)=> quotation.coinId = this.idCoin)
-    console.log(this.results)
+    this.quotationsService.getQuotations().subscribe((quotations) => {this.results = quotations.filter((quotation)=> quotation.coinId == this.idCoin)
+          this.graph = {
+        data: [
+            { x: this.results.map(function (obj:any) {return obj.dateCreation }) , y:this.results.map(function (obj:any) {return obj.price }) , type: 'scatter', mode: 'lines+points'},
+        ],
+        layout: {width: 400, height: 400, title: 'A Fancy Plot'}
+    };
+    let y1 = this.results[8]
+    this.graph2 = {
+      data: [
+        {
+          x: this.results.map(function (obj:any) {return obj.dateCreation }),
+          y: this.results.map(function (obj:any) {return obj.price }),
+          type: 'box'
+        }
+      ],
+      layout: {width: 400, height: 400, title: 'A Fancy Plot'}
+  };
     })
-  //   this.service.getCoinList().subscribe((coins )=> {
-  //   this.coin = coins.filter(coin => coin.nameCoin == this.route.snapshot.paramMap.get("idx"))[0]
   //   this.exchangeService.getQuotationForExchange(this.quotationForDay+this.coin.acronym+this.time).subscribe(response => {
   //     this.results = response
   //     this.results = this.results.Data.Data
